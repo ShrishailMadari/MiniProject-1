7728334da487d6aeceb3c81c8910cdac7159b19e
shrishail madari <shrishailmadari@gmail.com>
2025-01-06 16:51:31 +0530

    add these files

---
+++ IRCTC-Service/src/main/java/com/shyloostyle/irctcservice/IrctcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shyloostyle.irctcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrctcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrctcServiceApplication.class, args);
+	}
+
+}
---
+++ IRCTC-Service/src/test/java/com/shyloostyle/irctcservice/IrctcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shyloostyle.irctcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrctcServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
